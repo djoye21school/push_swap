d6e9e54d223287bc95eec45e308439d38a78e70a
Diedra Joye <djoye@ox-l5.21-school.ru>
2019-11-13 13:18:25 +0300

    home

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: djoye <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/09 15:50:25 by djoye             #+#    #+#             */
-/*   Updated: 2019/11/12 19:25:32 by djoye            ###   ########.fr       */
+/*   Updated: 2019/11/13 12:43:54 by djoye            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,46 +26,52 @@ int			equ(char const *s1, char const *s2)
 
 t_head		*rotate(t_head *head, char *buf)
 {
-	if ((equ("ss", buf) || equ("sa", buf)) && head->a && head->a->next)
-			sa(head->a);
-	if ((equ("ss", buf) || equ("sb", buf)) && head->b && head->b->next)
-			sa(head->b);
-	if (equ("pa", buf) && head->a)
+	int		c;
+
+	c = 0;
+	if ((equ("ss", buf) || equ("sa", buf)) && head->a && head->a->next && c++)
+		sa(head->a);
+	if ((equ("ss", buf) || equ("sb", buf)) && head->b && head->b->next && c++)
+		sa(head->b);
+	if (equ("pa", buf) && head->a && c++)
 		pa(head);
-	if (equ("pb", buf) && head->b)
+	if (equ("pb", buf) && head->b && c++)
 		pb(head);
-	if ((equ("rr", buf) || equ("ra", buf)) && head->a && head->a->next)
+	if ((equ("rr", buf) || equ("ra", buf)) && head->a && head->a->next && c++)
 		ra(head);
-	if ((equ("rr", buf) || equ("rb", buf)) && head->b && head->b->next)
+	if ((equ("rr", buf) || equ("rb", buf)) && head->b && head->b->next && c++)
 		rb(head);
-	if ((equ("rrr", buf) || equ("rra", buf)) && head->a && head->a->next)
+	if ((equ("rrr", buf) || equ("rra", buf)) && head->a && head->a->next && c++)
 		rra(head);
-	if ((equ("rrr", buf) || equ("rrb", buf)) && head->b && head->b->next)
+	if ((equ("rrr", buf) || equ("rrb", buf)) && head->b && head->b->next && c++)
 		rrb(head);
-	return (head);
+	printf("%d\n", c);
+	return (c == 0 ? NULL : head);
 }
 
 int			main(int argc, char **argv)
 {
-	char	buf[1024];
+	char	buf;
 	t_head	*head;
 	int		i;
-	int		len;
-	char	*tmp;
+	char	tmp[4];
 
 	if (argc == 1)
 		return (0);
-
 	i = 0;
-	head = NULL;
 	head = read_nb(argc, argv);
-	while((len = read(1, buf, 1024)) > 0)
+	while (read(1, &buf, 1) && i < 4)
 	{
-		tmp = buf;
-		while(*buf && *buf != '\n' && i++)
-			buf[i];
-		tmp[i] = '\0';
-		head = rotate(head, tmp);
+		if (buf == '\n')
+		{
+			tmp[i] = '\0';
+			printf("%s\n", tmp);
+			head = rotate(head, tmp);
+			i = 0;
+		}
+		else tmp[i++] = buf;
+		if (head == NULL || i > 3)
+			return (write(1, "error\n", 6) - 6);
 	}
 	if (if_sort(head->a, 1) == 1 && head->b == NULL)
 		write(1, "OK\n", 3);
